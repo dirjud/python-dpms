cbcef936d95b35074b0d3dffd1cb07e0a8c09689
Thiago Kenji Okada <thiagokokada@gmail.com>
2017-10-12 21:01:22 -0300

    Update README to include note about Python 2/3 compatibility

--- turnoff.py
+++ turnoff.py
@@ -1,8 +1,8 @@
 import dpms, time
 
-time.sleep(1) # ensure key presses from running this application are done
+time.sleep(1)  # ensure key presses from running this application are done
 
-d = dpms.DPMS() # to use the current display, or alternatively DPMS(":1")
+d = dpms.DPMS()  # to use the current display, or alternatively DPMS(":1")
 
 d.Enable()
 d.ForceLevel(dpms.DPMSModeOff)
