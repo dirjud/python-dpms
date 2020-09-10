c44a4245605927c67fbd2b5a3cadd963907b2a3e
Thiago Kenji Okada <thiagokokada@gmail.com>
2020-09-10 12:05:12 -0300

    Convert from CamelCase->snake_case

--- tests/test_pydpms.py
+++ tests/test_pydpms.py
@@ -9,103 +9,86 @@ import unittest
 class TestDpms(unittest.TestCase):
     def setUp(self):
         self.d = dpms.DPMS()
-        self.d.Enable()
+        self.d.enable()
         # Using xfvb or a monitor that does not support DPMS,
         # you may get X11 errors after some methods.
-        self.dpms_level, self.dpms_status = self.d.Info()
-        self.current_timeouts = self.d.GetTimeouts()
+        self.dpms_level, self.dpms_status = self.d.info()
+        self.current_timeouts = self.d.get_timeouts()
 
     def tearDown(self):
         if self.dpms_status:
-            self.d.Enable()
-            self.d.ForceLevel(self.dpms_level)
-            self.d.SetTimeouts(*self.current_timeouts)
+            self.d.enable()
+            self.d.force_level(self.dpms_level)
+            self.d.set_timeouts(*self.current_timeouts)
         else:
-            self.d.Disable()
+            self.d.disable()
 
     def test_init(self):
         with self.checkRaiseRegex(
-            Exception,
-            "Optional display keyword must be a string. e.g. ':0'"
+            Exception, "Optional display keyword must be a string. e.g. ':0'"
         ):
             dpms.DPMS(display=1)
 
-        with self.checkRaiseRegex(
-            Exception,
-            "Cannot open display"
-        ):
+        with self.checkRaiseRegex(Exception, "Cannot open display"):
             dpms.DPMS(display="invalid")
 
-    def test_Display(self):
-        self.assertEqual(
-            self.d.Display(),
-            os.environ["DISPLAY"]
-        )
+    def test_display(self):
+        self.assertEqual(self.d.display(), os.environ["DISPLAY"])
 
-    def test_QueryExtension(self):
-        extension = self.d.QueryExtension()
+    def test_query_extension(self):
+        extension = self.d.query_extension()
         self.assertTrue(type(extension) is tuple)
 
         self.assertTrue(type(extension[0]) is bool)
         self.assertTrue(type(extension[1]) is int)
         self.assertTrue(type(extension[2]) is int)
 
-    def test_Capable(self):
-        self.assertTrue(type(self.d.Capable()) is bool)
+    def test_capable(self):
+        self.assertTrue(type(self.d.capable()) is bool)
 
-    def test_GetTimeouts(self):
-        timeouts = self.d.GetTimeouts()
+    def test_get_timeouts(self):
+        timeouts = self.d.get_timeouts()
         self.assertTrue(type(timeouts) is tuple)
 
         for timeout in timeouts:
             self.assertTrue(type(timeout) is int)
 
-    def test_SetTimeouts(self):
+    def test_set_timeouts(self):
         with self.checkRaiseRegex(
-            Exception,
-            "Bad arguments. Should be \(int standby, int suspend, int off\)."
+            Exception, "Bad arguments. Should be \(int standby, int suspend, int off\)."
         ):
-            self.d.SetTimeouts(standby="600", suspend="600", off="600")
+            self.d.set_timeouts(standby="600", suspend="600", off="600")
 
-        self.d.SetTimeouts(standby=100, suspend=100, off=100)
-        self.assertEqual(
-            self.d.GetTimeouts(),
-            (100, 100, 100)
-        )
+        self.d.set_timeouts(standby=100, suspend=100, off=100)
+        self.assertEqual(self.d.get_timeouts(), (100, 100, 100))
 
-    def test_Enable(self):
-        self.d.Enable()
+    def test_enable(self):
+        self.d.enable()
         if self.dpms_status:
-            self.assertTrue(self.d.Info()[1])
+            self.assertTrue(self.d.info()[1])
 
-    def test_Disable(self):
-        self.d.Disable()
-        self.assertFalse(self.d.Info()[1])
+    def test_disable(self):
+        self.d.disable()
+        self.assertFalse(self.d.info()[1])
 
-    def test_ForceLevel(self):
-        current_level, _ = self.d.Info()
+    def test_force_level(self):
+        current_level, _ = self.d.info()
 
-        with self.checkRaiseRegex(
-            Exception,
-            "Bad arguments. Should be \(int level\)."
-        ):
-            self.d.ForceLevel(level="0")
+        with self.checkRaiseRegex(Exception, "Bad arguments. Should be \(int level\)."):
+            self.d.force_level(level="0")
 
-        with self.checkRaiseRegex(
-            Exception,
-            "Bad level."
-        ):
-            self.d.ForceLevel(level=1000)
+        with self.checkRaiseRegex(Exception, "Bad level."):
+            self.d.force_level(level=1000)
 
         if self.dpms_status:
             print("\nYour monitor may turnoff during this test", file=sys.stderr)
-            self.d.ForceLevel(level=dpms.DPMSModeOn)
-            self.d.ForceLevel(level=dpms.DPMSModeStandby)
-            self.d.ForceLevel(level=dpms.DPMSModeSuspend)
-            self.d.ForceLevel(level=dpms.DPMSModeOff)
+            self.d.force_level(level=dpms.DPMSModeOn)
+            self.d.force_level(level=dpms.DPMSModeStandby)
+            self.d.force_level(level=dpms.DPMSModeSuspend)
+            self.d.force_level(level=dpms.DPMSModeOff)
 
-    def test_Info(self):
-        info = self.d.Info()
+    def test_info(self):
+        info = self.d.info()
         self.assertTrue(type(info) is tuple)
 
         level, state = info
